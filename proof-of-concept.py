9f5206eac527a123ea6e64a04dc8ca1f213c87d9
Mark Schuegraf <mark.schuegraf@posteo.de>
2021-02-01 08:59:25 +0100

    Copied tasks for building the subjects and downloading their original bytecode over from method-mapping

--- proof-of-concept.py
+++ proof-of-concept.py
@@ -178,16 +178,6 @@ class ComputeCoverageStatistics(luigi.Task):
 # requires BuildSubject, DownloadOriginalBytecode, GenerateInputs
 
 
-# class BuildSubject(luigi.Task):
-
-
-# class DownloadOriginalBytecode(luigi.Task):
-
-
-# class GenerateInputs(luigi.Task):
-# requires RunTribbleGenerationMode
-
-
 class RunTribbleGenerationMode(luigi.Task):
     """Generates inputs with tribble using a transformed grammar."""
     format: str = luigi.Parameter(description="The name of the format directory (e.g. json)", positional=False)
@@ -272,6 +262,34 @@ class GradleTask(object):
         if commands:
             invocation.extend(commands)
         return invocation
+
+
+class BuildSubject(luigi.Task, GradleTask):
+    """Builds the given subject and copies it into the working directory."""
+    subject_name: str = luigi.Parameter(description="The name of the subject to build", positional=False)
+
+    def output(self):
+        return luigi.LocalTarget(work_dir / "tools" / "subjects" / self.subject_name / f"{self.subject_name}-subject.jar")
+
+    def run(self):
+        subprocess.run(self.gradlew("build", "-p", self.subject_name), check=True, cwd=tool_dir / "subjects", stdout=subprocess.DEVNULL)
+        artifact = tool_dir / "subjects" / self.subject_name / "build" / "libs" / f"{self.subject_name}-subject.jar"
+        os.makedirs(os.path.dirname(self.output().path), exist_ok=True)
+        shutil.copy(str(artifact), self.output().path)
+
+
+class DownloadOriginalBytecode(luigi.Task, GradleTask):
+    """Downloads the unmodified bytecode of the subject and places it into the working directory."""
+    subject_name: str = luigi.Parameter(description="The name of the subject to build")
+
+    def output(self):
+        return luigi.LocalTarget(work_dir / "tools" / "subjects" / self.subject_name / f"{self.subject_name}-original.jar")
+
+    def run(self):
+        subprocess.run(self.gradlew("downloadOriginalJar", "-p", self.subject_name), check=True, cwd=tool_dir / "subjects", stdout=subprocess.DEVNULL)
+        artifact = tool_dir / "subjects" / self.subject_name / "build" / "libs" / f"{self.subject_name}-original.jar"
+        os.makedirs(os.path.dirname(self.output().path), exist_ok=True)
+        shutil.copy(str(artifact), self.output().path)
 
 
 class BuildTribble(luigi.Task, GradleTask):
