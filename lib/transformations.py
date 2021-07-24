29aee45d30ae1ac5acea1b46e3469438810de4a0
Mark Schuegraf <s9mkscue@stud.uni-saarland.de>
2021-07-24 23:50:26 +0200

    refactored transformations

--- lib/transformations.py
+++ lib/transformations.py
@@ -16,28 +16,27 @@ from lib import tooling
 from lib import work_dir
 
 
-class TransformGrammar(luigi.Task, metaclass=ABCMeta):
-    """Transforms the input grammar using tribble's transformation mode."""
+class TransformGrammarWithTribble(luigi.Task, metaclass=ABCMeta):
     format: str = luigi.Parameter(description="The format specified by the input grammar.")
     resources = {"ram": 16}
 
     @property
     @abstractmethod
-    def transformation_mode(self):
+    def tribble_transformation_mode(self):
         raise NotImplementedError("Must specify a transformation mode to use for transformation!")
 
     @property
     @abstractmethod
-    def prerequisite_task(self):
+    def prerequisite_transformation_task(self):
         raise NotImplementedError("Must specify the preconditions that must hold before transformation!")
 
     @property
     @abstractmethod
-    def compound_transformation(self):
+    def enclosing_compound_transformation(self):
         raise NotImplementedError("Must specify the overall conversion procedure that this is a substep of!")
 
     def requires(self):
-        return tooling.BuildTribble(), self.clone(self.prerequisite_task)
+        return tooling.BuildTribble(), self.clone(self.prerequisite_transformation_task)
 
     def run(self):
         tribble_jar = self.input()[0].path
@@ -57,28 +56,27 @@ class TransformGrammar(luigi.Task, metaclass=ABCMeta):
                     f"--output-grammar-file={out}",
                     f"--loading-strategy={loading_strategy}",
                     "--storing-strategy=marshal",
-                    f"--mode={self.transformation_mode}",
+                    f"--mode={self.tribble_transformation_mode}",
                     ]
             logging.info("Launching %s", " ".join(args))
             subprocess.run(args, check=True, stdout=subprocess.DEVNULL)
 
 
-class ElementaryTransformGrammar(TransformGrammar, metaclass=ABCMeta):
+class ElementaryTransformGrammarWithTribble(TransformGrammarWithTribble, metaclass=ABCMeta):
     def output(self):
         """If the transformation is elementary, store the intermediate grammar in a subdirectory of the target path."""
         return luigi.LocalTarget(
-            work_dir / "transformed-grammars" / self.format / self.compound_transformation / self.transformation_mode / self.format)
+            work_dir / "transformed-grammars" / self.format / self.enclosing_compound_transformation / self.tribble_transformation_mode / self.format)
 
 
-class CompoundTransformGrammar(TransformGrammar, metaclass=ABCMeta):
+class CompoundTransformGrammarWithTribble(TransformGrammarWithTribble, metaclass=ABCMeta):
     def output(self):
         """If the transformation is compound, store the output grammar directly in the transformation directory."""
         return luigi.LocalTarget(
-            work_dir / "transformed-grammars" / self.format / self.compound_transformation / self.format)
+            work_dir / "transformed-grammars" / self.format / self.enclosing_compound_transformation / self.format)
 
 
 class ProduceOriginalGrammar(luigi.ExternalTask):
-    """Fetches the original grammar modeling a certain format."""
     format: str = luigi.Parameter(description="The format specified by the input grammar.")
 
     def output(self):
@@ -90,29 +88,29 @@ Chomsky.
 """
 
 
-class TransformChomskyPre1(ElementaryTransformGrammar):
+class TransformGrammarChomskyStep1(ElementaryTransformGrammarWithTribble):
     @property
-    def transformation_mode(self):
+    def tribble_transformation_mode(self):
         return "backus-naur-formalizer"
 
     @property
-    def prerequisite_task(self):
+    def prerequisite_transformation_task(self):
         return ProduceOriginalGrammar
 
     @property
-    def compound_transformation(self):
+    def enclosing_compound_transformation(self):
         return "chomsky-normal-form"
 
 
-class TransformChomsky(CompoundTransformGrammar):
+class TransformGrammarChomsky(CompoundTransformGrammarWithTribble):
     @property
-    def transformation_mode(self):
+    def tribble_transformation_mode(self):
         return "chomsky-normal-formalizer"
 
     @property
-    def prerequisite_task(self):
-        return TransformChomskyPre1
+    def prerequisite_transformation_task(self):
+        return TransformGrammarChomskyStep1
 
     @property
-    def compound_transformation(self):
+    def enclosing_compound_transformation(self):
         return "chomsky-normal-form"
