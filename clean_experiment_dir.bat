050e570558959054a6ed97808c7359b5f953240e
Mark Schuegraf <s9mkscue@stud.uni-saarland.de>
2021-08-02 19:35:22 +0200

    Scripts for cleaning experiment dir after a successful luigi run

---
+++ clean_experiment_dir.bat
@@ -0,0 +1,3 @@
+Remove-Item -path C:\Users\schue\Documents\linux_projects\master_experiment\inputs -recurse
+Remove-Item -path C:\Users\schue\Documents\linux_projects\master_experiment\transformed-grammars -recurse
+Remove-Item -path C:\Users\schue\Documents\linux_projects\master_experiment\results -recurse
---
+++ clean_experiment_dir.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+rm -rf /Users/schuegrafm/Documents/master_experiment/inputs
+rm -rf /Users/schuegrafm/Documents/master_experiment/transformed-grammars
+rm -rf /Users/schuegrafm/Documents/master_experiment/results
