cfdb2f3bddd1998617ab1fce2fa4a5d148c8215d
Mark Schuegraf <mark.schuegraf@posteo.de>
2021-08-13 00:41:36 +0200

    configured the experiment to test the revised evaluation tasks

--- experiments.py
+++ experiments.py
@@ -11,18 +11,19 @@ import sys
 import luigi
 from luigi.util import requires
 
-from lib import execution
+from lib import evaluation
 
 
-@requires(execution.RunSubjectAndProduceCoverageReport)
+@requires(evaluation.EvaluateCoverage, evaluation.EvaluateCoverageGrowthRate)
 class Experiment(luigi.WrapperTask):
     """Attempts to find a relationship between grammar transformations and coverage metrics."""
-    transformation_mode: str = luigi.Parameter(description="The tribble transformation mode to use.")
     language: str = luigi.Parameter(description="The language specified by the input grammar.")
+    transformation_mode: str = luigi.Parameter(description="The tribble transformation mode to use.")
     generation_mode: str = luigi.Parameter(description="The tribble generation mode to use.")
+    subject_name: str = luigi.Parameter(description="The name of the subject to run.")
     run_number: int = luigi.IntParameter(description="The run number of the produced input set.")
     language_seed: int = luigi.IntParameter(description="The seed from which seeds for this language are derived.")
-    subject_name: str = luigi.Parameter(description="The name of the subject to run.")
+    total_number_of_runs: int = luigi.IntParameter(description="The number of runs to conduct per configuration.")
 
 
 if __name__ == '__main__':
