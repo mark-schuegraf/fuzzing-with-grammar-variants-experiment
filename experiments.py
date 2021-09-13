2740b76ce6f2811e98f4c8e7b6a50207fca638ea
Mark Schuegraf <mark.schuegraf@posteo.de>
2021-09-13 21:11:43 +0200

    dropped useless experiment wrapper task

--- experiments.py
+++ experiments.py
@@ -2,25 +2,17 @@
 # -*- coding: utf-8 -*-
 
 """
-This module contains luigi tasks to run the grammar transformation experiments.
+This module represents the main entry point to running the grammar transformation experiments.
 """
 
 import logging
 import sys
 
 import luigi
-from luigi.util import requires
 
 from lib import task_dispatch
-
-
-# TODO import DispatchLanguages directly, if there ends up being no other behavior that this task is responsible for
-@requires(task_dispatch.DispatchLanguages)
-class Experiment(luigi.WrapperTask):
-    """Attempts to find a relationship between grammar transformations and coverage metrics."""
-
 
 if __name__ == '__main__':
     logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s", datefmt="%d.%m.%Y %H:%M:%S", level=logging.INFO,
                         stream=sys.stdout)
-    luigi.run(main_task_cls=Experiment)
+    luigi.run(main_task_cls=task_dispatch.DispatchLanguages)
