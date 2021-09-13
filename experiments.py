de1df177a108399fc8a483bfb7847d4c53e94106
Mark Schuegraf <mark.schuegraf@posteo.de>
2021-09-13 21:07:18 +0200

    email notifications for failed tasks

--- experiments.py
+++ experiments.py
@@ -23,5 +23,4 @@ class Experiment(luigi.WrapperTask):
 if __name__ == '__main__':
     logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s", datefmt="%d.%m.%Y %H:%M:%S", level=logging.INFO,
                         stream=sys.stdout)
-    # TODO: add run monitoring later
     luigi.run(main_task_cls=Experiment)
