8625b557d0cc932da23dd7ff84e40545f47b7c68
Mark Schuegraf <mark.schuegraf@posteo.de>
2021-09-15 15:19:42 +0200

    removed useless scripts

--- clean_experiment_dir.bat
+++
@@ -1,3 +0,0 @@
-Remove-Item -path C:\Users\schue\Documents\linux_projects\master_experiment\inputs -recurse
-Remove-Item -path C:\Users\schue\Documents\linux_projects\master_experiment\transformed-grammars -recurse
-Remove-Item -path C:\Users\schue\Documents\linux_projects\master_experiment\results -recurse
--- clean_experiment_dir.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-rm -rf /Users/schuegrafm/Documents/master_experiment/inputs
-rm -rf /Users/schuegrafm/Documents/master_experiment/transformed-grammars
-rm -rf /Users/schuegrafm/Documents/master_experiment/results
