f52973653759ecb5ed64429fc30f086b853cd153
Mark Schuegraf <mark.schuegraf@posteo.de>
2021-09-18 14:34:30 +0200

    added time measurement and logging to run experiments script

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 ./start_luigi_daemon.sh
-python3 experiments.py --random-seed=42 --total-number-of-runs=100 --workers=64
+time python3 experiments.py --random-seed=42 --total-number-of-runs=100 --workers=64 2>&1 | tee luigi.log
