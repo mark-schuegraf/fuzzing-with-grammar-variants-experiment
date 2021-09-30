24e958b82f7f970d2568f0462c1e092ae8916204
Mark Schuegraf <mark.schuegraf@posteo.de>
2021-09-30 21:38:05 +0200

    lowered the number of runs to reduce experiment duration

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 ./start_luigi_daemon.sh
-time python3 experiments.py --random-seed=42 --total-number-of-runs=100 --workers=64 2>&1 | tee luigi.log
+time python3 experiments.py --random-seed=42 --total-number-of-runs=50 --workers=64 2>&1 | tee luigi.log
